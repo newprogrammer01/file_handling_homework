df51abcd64670e598e2ca9c5ab4e123ad214d251
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-11-10 11:34:39 +0500

    0303

--- file03.py
+++ file03.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    l = []
+    for i in data:
+        if i.isdigit():
+            l.append(i)
+    return l
 # Read data from file
+f=open('txt_file/data03.txt').read()
+print(main(f))
+
+
+
+
