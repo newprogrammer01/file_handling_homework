e4df5a763668762b4bf9b203d13166c6e8965e08
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-11-10 14:24:51 +0500

    05

--- file05.py
+++ file05.py
@@ -6,5 +6,19 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    a=[]
     
-# Read data from file
+    for i in data:
+        if i.isalpha():
+            a.append(i)
+        if i=='\n':
+            a.append(' ')
+    l1=len(a)
+    l2=len(data)-l1
+    r=[l2,l1]
+    return r
+f=open('txt_file/data05.txt').read()
+print(main(f))
+
+    
+
