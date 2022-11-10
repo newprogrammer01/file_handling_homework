eb32447d46cc12ea9f9def3d5e00eb8996ace260
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-11-10 12:10:19 +0500

    04

--- file04.py
+++ file04.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    l=[]
+    for i in data:
+        if i.isalpha():
+           l.append(i)
+
+       
+           
+    return l 
 # Read data from file
+f=open("txt_file/data04.txt").read()
+print(main(f))
